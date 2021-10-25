f55d32bf533f3d7e2e6d6a3620a1ae5fd93883e7
amanuelgezahange12 <69604107+amanuelgezahange12@users.noreply.github.com>
2021-10-24 23:36:53 -0500

    commited

--- src/App.js
+++ src/App.js
@@ -10,18 +10,30 @@ function App() {
       updateState("");
     }
   };
-
-  useEffect(() => convertToAmharic());
-
-  const convertToAmharic = () => {
-    axios
-      .get("https://amh-translator.herokuapp.com/" + state)
-      .then((doc) => updateState(doc.data));
+  const sumbit = () => {
+    if (state.length > 1) {
+      axios.post("https://amh-translator.herokuapp.com", { word: state }).then((doc) => {
+        axios.get("https://amh-translator.herokuapp.com/" + state).then((doc) => {
+          console.log(doc);
+          let data = doc.data;
+          if (typeof data === "string") {
+            updateState(data);
+          }
+          console.log(data);
+        });
+      });
+    }
   };
-  const clearHandle=()=>{
-    setState("")
-    updateState("")
-  }
+
+  const clearHandle = () => {
+    axios
+      .delete("https://amh-translator.herokuapp.com")
+      .then(() => {
+        setState("");
+        updateState("");
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="container">
       <h1 className="colorize2">
@@ -36,6 +48,7 @@ function App() {
           placeholder="Search"
           onChange={(e) => wordEventHandler(e)}
         />
+        <button onClick={sumbit}>search</button>
         <button onClick={clearHandle}>clear</button>
         <div className="colorize1">
           {" "}
