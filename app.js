9fe3819fed870794974e3452a8d7395dd1722c37
amanuelgezahange12 <69604107+amanuelgezahange12@users.noreply.github.com>
2021-10-26 22:20:08 -0500

    commited

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const dotenv = require('dotenv');
 dotenv.config();
 const wordRouter = require('./routes/word');
 
 const cors = require('cors');
+
+const url = process.env.DATABASE_ACCESS;
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
 
 
@@ -14,6 +18,26 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/", function (req, res, next) {
+    let connection;
+    if (!connection) {
+      client.connect(function (err, data) {
+          if(err) {
+              console.log("Error connecting to server")
+          }else {
+              console.log(data,"connect successfully");
+          }
+      
+        connection = client.db("atlasDB");
+        req.db = connection;
+        next();
+      });
+    } else {
+      req.db = connection;
+      next();
+    }
+  });
+
 app.use('/', wordRouter);
  
 
--- routes/word.js
+++ routes/word.js
@@ -1,22 +1,46 @@
 const express = require("express");
 const translate = require("translate");
+const { ObjectID } = require("bson");
 const router = express.Router();
+router.use(express.static(`main`));
 
 // get all farmers
 //localhost:4000/farmers
 
-router.get("/:word", (req, res, next) => {
+// router.post("/", (req, res) => {
+//   req.db
+//     .collection("words")
+//     .insertOne({word:req.body.word})
+//     .then((doc) => res.json({ status: "success" }))
+//     .catch((err) => res.json({ status: "fail" }));
+// });
+
+// router.get("/", (req, res) => {
+//   req.db
+//     .collection("words")
+//     .find()
+//     .toArray()
+//     .then((doc) => res.json(doc));
+// });
+
+router.get("/:word", (req, res) => {
   async function translateString(str, translateTo) {
     translate.engine = "google"; // Or "yandex", "libre", "deepl"
     translate.key = process.env.GOOGLE_KEY;
     const translateString = await translate(str, translateTo);
-    // console.log(translateString);
-    return translateString
+    return translateString;
   }
 
-  translateString(req.params.word, "Amh").then((doc)=>res.json(doc))
-  // next()
+  translateString(req.params.word, "Amh").then((doc) => res.json(doc));
 });
+
+// router.delete("/", (req, res) => {
+//   req.db
+//     .collection("words")
+//     .deleteMany({})
+//     .then((doc) => res.json({ status: "success" }))
+//     .catch((err) => res.json({ status: "fail" }));
+// });
 
 module.exports = router;
 //
